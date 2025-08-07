79a54fdeb6156f33b4191ca8ff55185e243da43d
JL-Agbalo <johnlenardagbalo@gmail.com>
2025-08-07 06:28:46 -0700

    Implement article editing and updating functionality; enhance error handling in forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,19 @@ class ArticlesController < ApplicationController
    @articles = Article.all
  end
 
+ def edit
+   @article = Article.find(params[:id])
+ end
+
+ def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: "Article was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+ end
+
  def new
    @article = Article.new
  end
@@ -16,8 +29,18 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article, notice: "Article was successfully created."
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, notice: "Article was successfully deleted."
+  rescue ActiveRecord::RecordNotFound
+    redirect_to articles_path, alert: "Article not found."
+  rescue StandardError => e
+    redirect_to articles_path, alert: "An error occurred: #{e.message}"
   end
 
   private
