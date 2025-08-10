f39bfe2e1513c3558521e54fc61e84cd3b717c43
JL-Agbalo <johnlenardagbalo@gmail.com>
2025-08-09 22:53:21 -0700

    Refactor article forms and layouts; implement flash message rendering and add form partial for article creation and editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,22 @@
 class ArticlesController < ApplicationController
- def show
-   @article = Article.find(params[:id])
- end
+before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+
+def show
+end
 
  def index
    @articles = Article.all
  end
 
  def edit
-   @article = Article.find(params[:id])
  end
 
  def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
+     if @article.update(article_params)
       redirect_to @article, notice: "Article was successfully updated."
-    else
+     else
       render :edit, status: :unprocessable_entity
-    end
+     end
  end
 
  def new
@@ -34,7 +33,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path, notice: "Article was successfully deleted."
   rescue ActiveRecord::RecordNotFound
@@ -47,5 +45,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+       @article = Article.find(params[:id])
   end
 end
